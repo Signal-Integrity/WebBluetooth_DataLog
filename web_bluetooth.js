a83f4f860906585f93598ac8d07c62a329b3e940
isbs <63434466+Signal-Integrity@users.noreply.github.com>
2020-06-14 17:25:50 +0900

    Update web_bluetooth.js

--- web_bluetooth.js
+++ web_bluetooth.js
@@ -2,7 +2,7 @@
 const UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
 const UART_SERVICE_CHARACTERISTICS_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
 
-async function connect()
+function connect()
 {
     navigator.bluetooth.requestDevice({
         acceptAllDevices : true,
